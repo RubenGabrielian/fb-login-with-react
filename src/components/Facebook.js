1cc4d1810c80692eefad3beaf292200b87b8a99c
RubenGabrielian <gabrielianruben@gmail.com>
2020-04-20 11:48:47 +0400

    first commit

---
+++ src/components/Facebook.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import FacebookLoginBtn from 'react-facebook-login'
+
+
+export default class LoginFacebook extends Component {
+    state = {
+        auth: false,
+        name: '',
+        picture: ''
+    }
+
+    componentClicked = () => {
+        console.log('fb clicked')
+    }
+
+    responseFacebook = (response) => {
+        console.log(response)
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url
+        })
+    }
+
+    render () {
+        let facebookData
+        this.state.auth ?
+            facebookData = (
+                <div>
+                    <img src={this.state.picture} />
+                    <h1>{`Hi ${this.state.name}`}</h1>
+                </div>
+            ) : 
+            facebookData = (
+                <FacebookLoginBtn
+                appId="1112476095779315"
+                autoLoad={true}
+                fields="name,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} 
+                icon="fa-facebook"
+                />
+            )
+
+
+        return (
+            <>
+                {facebookData}
+                <a href={`https://www.facebook.com/sharer.php?u=https:localhost:3000?imageurl=${this.state.picture}}`}>Share</a>
+            </>
+        )   
+    }
+}
